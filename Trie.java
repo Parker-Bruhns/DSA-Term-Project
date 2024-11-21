5bd32192343d47d0e95eb452378bd86f97dc204a
Parker Bruhns <pbruhns2023@my.fit.edu>
2024-11-21 16:27:36 -0500

    Added constructors and inner classes to trie class

--- Trie.java
+++ Trie.java
@@ -1,6 +1,33 @@
 public class Trie {
+   public class Node {
+      private char data;
+      private Node next;
+      private Suffix last;
+
+      public Node (char data) {
+         this.data = data;
+      }
+
+      public Node (char data, Node next) {
+         this.data = data;
+         this.next = next;
+      }
+
+      public Node (char data, Suffix last) {
+         this.data = data;
+         this.last = last;
+      }
+   }
+
+   public class Suffix {
+
+   }
+
+   // Trie class
+   private Node root;
    
    public Trie () {
+      this.root = new Node();
 
    }
 
