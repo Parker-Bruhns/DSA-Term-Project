1a4ac2029f2d7be22d5f9a535e45956ad4f7a2af
Parker Bruhns <pbruhns2023@my.fit.edu>
2024-11-24 02:22:14 -0500

    Testing code in progress

--- Test.java
+++ Test.java
@@ -1,5 +1,9 @@
+import java.io.IOException;
+
 public class Test {
-   public static void main(String[] args) {
+   public static void main(String[] args) throws IOException {
       Trie trie = new Trie();
+      trie.loadWordsFromFile("Input Files/words.txt");
+      trie.printAllWords();
    }
 }
